2bef623faeebdeaa422c2b0ad916a7766493b131
Mei Eisenbach <mei.eisenbach@gmail.com>
2018-12-13 16:57:26 -0800

    feat: Add code from ML Pipeline notebook

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,78 @@
 import sys
+import pandas as pd
+from sqlalchemy import create_engine
+import re
+import pickle
 
+from nltk.tokenize import word_tokenize
+from nltk.stem.wordnet import WordNetLemmatizer
+
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import classification_report, hamming_loss
 
 def load_data(database_filepath):
-    pass
+    engine = create_engine('sqlite:///'+database_filepath)
+    df = pd.read_sql_table('messages', engine)    
+    
+    X = df.message
+    y = df.drop(['id', 'message', 'original', 'genre'], axis='columns')
+    category_names = y.columns
+    
+    return X, y, category_names
 
 
 def tokenize(text):
-    pass
-
+    # remove punctuation
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text)
+    
+    # tokenize
+    tokens = word_tokenize(text)
+    
+    # initalize lemmatizer
+    lemmatizer = WordNetLemmatizer()
+    
+    clean_tokens = []
+    for token in tokens:
+        clean_tokens.append(lemmatizer.lemmatize(token).lower().strip())
+        
+    return clean_tokens
 
 def build_model():
-    pass
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(AdaBoostClassifier()))
+    ])
+    
+    parameters = {
+        'vect__ngram_range':[(1,1), (1,2)],
+        'clf__estimator__learning_rate':[0.75, 1.0],
+        'clf__estimator__n_estimators':[50, 75]
+    }
+    
+    cv = GridSearchCV(pipeline, parameters, scoring='roc_auc', cv=3)
+    return cv
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    Y_pred = pd.DataFrame(model.predict(X_test))
+    
+    for i in range(Y_test.shape[1]):
+        print('Column {}, {}'.format(i, category_names[i]))
+        print(classification_report(Y_test.iloc[:,i], Y_pred.iloc[:,i]))
+    
+    print(hamming_loss(Y_test, Y_pred))
+    return None
 
 
 def save_model(model, model_filepath):
-    pass
+    with open('classifier.pkl', 'wb') as f:
+        pickle.dump(model, f)    
+    return None
 
 
 def main():
